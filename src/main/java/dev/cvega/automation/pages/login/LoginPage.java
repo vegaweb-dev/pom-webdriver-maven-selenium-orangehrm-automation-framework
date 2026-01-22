3b488a13e4fec3f39c89d4384479a558d419b127
cvega <carlosjuniorrvega@hotmail.com>
2026-01-22 15:18:52 -0500

    refactor(LoginPage): move isDashboardDisplayed method for SRP. CV
    - moved isDashboardDisplayed method to DashboardPage class.
    - moved dashboardTitle locator to Dashboard class.
    - changed void return type to DashboardPage in loginAs method.
    - added return statement to loginAs method.

--- src/main/java/dev/cvega/automation/pages/login/LoginPage.java
+++ src/main/java/dev/cvega/automation/pages/login/LoginPage.java
@@ -1,6 +1,7 @@
 package dev.cvega.automation.pages.login;
 
 import dev.cvega.automation.pages.BasePage;
+import dev.cvega.automation.pages.dashboard.DashboardPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
@@ -12,7 +13,6 @@ public class LoginPage extends BasePage {
     private final By usernameInput = By.name("username");
     private final By passwordInput = By.name("password");
     private final By loginButton = By.cssSelector("button[type='submit']");
-    private final By dashboardTitle = By.xpath("//h6[contains(@class, 'topbar-header') and text()='Dashboard']");
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -23,11 +23,13 @@ public class LoginPage extends BasePage {
      *
      * @param username The account identifier.
      * @param password Secret credential of the account.
+     * @return A new instance of {@link DashboardPage} representing the landing page after login.
      */
-    public void loginAs(String username, String password) {
+    public DashboardPage loginAs(String username, String password) {
         enterUsername(username);
         enterPassword(password);
         clickLogin();
+        return new DashboardPage(driver);
     }
 
     public void enterUsername(String username) {
@@ -41,16 +43,4 @@ public class LoginPage extends BasePage {
     public void clickLogin() {
         click(loginButton);
     }
-
-    /**
-     * Acts as a sync point to confirm successful login.
-     * It waits for the 'dashboardTitle' locator defined in the Page Object.
-     * * @return true if the element is found within the configured timeout.
-     *
-     * @throws TimeoutException if the dashboard fails to load.
-     */
-    public boolean isDashboardDisplayed() {
-        return find(dashboardTitle).isDisplayed();
-    }
-
 }
