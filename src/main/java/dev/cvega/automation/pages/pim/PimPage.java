588b02565831246c302b4b605b7022ebabd1852a
cvega <carlosjuniorrvega@hotmail.com>
2026-01-26 16:22:08 -0500

    feat(PimPage): implement PimPage class for fluent chaining. CV

--- src/main/java/dev/cvega/automation/pages/pim/PimPage.java
+++ src/main/java/dev/cvega/automation/pages/pim/PimPage.java
@@ -1,4 +1,16 @@
 package dev.cvega.automation.pages.pim;
 
-public class PimPage {
+import dev.cvega.automation.pages.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class PimPage extends BasePage {
+    private final By pimPageTitle = By.xpath("//h6[contains(@class, 'header') and normalize-space(.)='PIM']");
+
+    public PimPage(WebDriver driver) {
+        super(driver);
+        if (!isElementDisplayed(pimPageTitle)) {
+            throw new IllegalStateException("This is not PimPage. Current URL: " + driver.getCurrentUrl());
+        }
+    }
 }
