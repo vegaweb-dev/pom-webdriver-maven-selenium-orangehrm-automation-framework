e10dd6eac1f11f3013e8d8658ba461136e6bb7c9
cvega <carlosjuniorrvega@hotmail.com>
2026-01-26 16:30:50 -0500

    feat(DashboardToPinNavigationTest): implement fluent navigation and E2E validation for PIM. CV

--- src/test/java/tests/dashboard/DashboardToPinNavigationTest.java
+++ src/test/java/tests/dashboard/DashboardToPinNavigationTest.java
@@ -1,13 +1,16 @@
 package tests.dashboard;
 
 import base.BaseTest;
-import dev.cvega.automation.pages.dashboard.DashboardPage;
+import dev.cvega.automation.pages.login.LoginPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DashboardToPinNavigationTest extends BaseTest {
     @Test
     public void navigateFromDashboardToPim_shouldDisplayPimPage() {
-        DashboardPage dashboardPage = new DashboardPage(driver);
-        dashboardPage.navigateToPim();
+        new LoginPage(driver).loginAs("Admin", "admin123").navigateToPim();
+        String currentUrl = driver.getCurrentUrl();
+        Assert.assertNotNull(currentUrl, "Url should not be null");
+        Assert.assertTrue(currentUrl.contains("viewEmployeeList"), "Url does not match PIM page after clicking on PIM");
     }
 }
